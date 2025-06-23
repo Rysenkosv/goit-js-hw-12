e8d289dc4dc486bd9a7190abcbef670845b40cac
Serhii Rysenko <rysenkosv@gmail.com>
2025-06-23 16:03:38 +0300

    init start

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,14 +3,17 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '50874397-c6e917ec67a94e5ad530eed0e';
 
-export function fetchImages(query) {
-  return axios.get(BASE_URL, {
+export async function fetchImages(query, page = 1) {
+  const response = await axios.get(BASE_URL, {
     params: {
       key: API_KEY,
       q: query,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
+      page,
+      per_page: 15,
     },
   });
+  return response;
 }
